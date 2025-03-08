cf85e9a8d709be7a25c31af251e0f24c5f0b1e53
Carina Huang <huangc24@students.cs.ubc.ca>
2025-03-08 13:51:07 -0800

    Implement Pill class

--- src/model/Pill.java
+++ src/model/Pill.java
@@ -2,6 +2,37 @@ package model;
 
 import java.time.LocalTime;
 
-public class Pill  {
+public class Pill {
+    private String name;
+    private int dosage;
+    private int frequencyDay;
+    private String noteString;
+
+    public Pill(String name, int dosage, int freq) {
+        this.name = name;
+        this.dosage = dosage;
+        frequencyDay = freq;
+        noteString = "";
+    }
+
+    public void addNote(String note) {
+        noteString = note;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDosage() {
+        return dosage;
+    }
+
+    public int getFreq() {
+        return frequencyDay;
+    }
+
+    public String getNote() {
+        return noteString;
+    }
 
 }
