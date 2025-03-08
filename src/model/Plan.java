69cf4609093c6dac094d775ffb6090aa887023d2
Hannah <banana_0316@qq.com>
2025-03-08 12:58:28 -0800

    rearrange class

--- src/model/Plan.java
+++ src/model/Plan.java
@@ -3,12 +3,5 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class Plan {
-    String planName;
-    ArrayList<Pill> pillsList ;
-    LocalDate startDate;
-    LocalDate endDate;
-    
-
-
     
 }
