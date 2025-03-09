3b373fb4c08383c222758b135c9e8f90139b99d3
Hannah <banana_0316@qq.com>
2025-03-08 16:06:49 -0800

    updated AlarMed

--- src/main/AlarMed.java
+++ src/main/AlarMed.java
@@ -21,7 +21,7 @@ public class AlarMed {
      public void initiate() {
 
         this.scanner = new Scanner(System.in);
-        patient = new Patient("", LocalDate.now(), LocalDate.now());
+        patient = new Patient("");
         this.isProgramRunning = true;
      }
 
@@ -31,7 +31,44 @@ public class AlarMed {
         this.processMenuCommands(input);
      }
 
-     public 
+     public void addNewPatient(){
+        System.out.println("Please enter the patient name:");
+        String name = this.scanner.nextLine();
+        patient = new Patient(name);
+        System.out.println("\n Welcome patient");
+
+     }
+
+     public void addNewPill(){
+        System.out.println("Please enter the medication name:");
+        String name = this.scanner.nextLine();
+        System.out.println("Please enter the medication dosage:");
+        String dosage = this.scanner.nextLine();
+        System.out.println("Please enter the medication frequency (1= once per day, 7=weekly):");
+        String freq = this.scanner.nextLine();
+        System.out.println("Please enter the start date (yyyy-MM-dd):");
+        String start = this.scanner.nextLine();
+        System.out.println("Please enter the end date (yyyy-MM-dd):");
+        String end = this.scanner.nextLine();
+        LocalDate startDate =  LocalDate.parse(start);
+        LocalDate endDate = LocalDate.parse(end);
+        patient.addPill(name, dosage, freq, start, end);
+     }
+
+     public void deletePill() {
+        System.out.println("Please enter the medication to be removed:");
+        String name = this.scanner.nextLine();
+        Pill pill = getPillInfo(name);
+        System.out.println("Confirm? y/n");
+        String con = this.scanner.nextLine();
+        if (con =="y") {
+            removePill(pill);
+        }
+        System.out.println("Pill Removed");
+        
+     }
+    
+     public void 
 
     
 }
